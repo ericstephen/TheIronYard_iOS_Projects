2c3e1fab2e54e74d4b70fb45fdf85238e64dbe82
ericstephen <EricWilliams@Macintosh-2.local>
2014-07-23 10:15:54 -0400

    create list app

---
+++ List App/List App/MFLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MFLAppDelegate.h
+//  List App
+//
+//  Created by Eric Williams on 7/23/14.
+//  Copyright (c) 2014 Eric Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ List App/List App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  List App
+//
+//  Created by Eric Williams on 7/23/14.
+//  Copyright (c) 2014 Eric Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MFLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MFLAppDelegate class]));
+    }
+}
