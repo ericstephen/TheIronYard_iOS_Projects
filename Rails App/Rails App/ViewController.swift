242bea9e1bde097979e649b92dcd6f97f1917709
ericstephen <EricWilliams@Erics-Macbook-Pro.local>
2014-09-02 09:38:13 -0400

    Rails App - Retrieving ALL Data From JSON

--- Rails App/Rails App/ViewController.swift
+++ Rails App/Rails App/ViewController.swift
@@ -8,10 +8,43 @@
 
 import UIKit
 
+let API = "http://ancient-taiga-9634.herokuapp.com/"
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var contentField: UITextField!
+    
+    @IBAction func postNew(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func getMyPosts(sender: AnyObject) {
+    }
+    
+    
+    @IBAction func getAllPosts(sender: AnyObject) {
+        
+        let allPostsURL = API + "posts"
+        
+        let request = NSURLRequest(URL: NSURL(string: allPostsURL))
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            
+            let posts = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: nil) as NSArray
+            
+            println(posts)
+        }
+    }
+
+    
+    
                             
     override func viewDidLoad() {
+
         super.viewDidLoad()
+        
+    
         // Do any additional setup after loading the view, typically from a nib.
     }
 
