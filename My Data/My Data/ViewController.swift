9b650dfc800a9c052683fb9e2f87d5c3a8f08e95
ericstephen <EricWilliams@Erics-Macbook-Pro.local>
2014-09-02 11:21:14 -0400

    My Data - Fetch Request Using Core Data

--- My Data/My Data/ViewController.swift
+++ My Data/My Data/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
+import CoreData
+
 class ViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var appD = UIApplication.sharedApplication().delegate as AppDelegate
+        
+        var fetchRequest = NSFetchRequest()
+        
+        var entity = NSEntityDescription.entityForName("Post", inManagedObjectContext: appD.managedObjectContext)
+        
+        fetchRequest.entity = entity
+        
+        var predicate = NSPredicate(format: "user = 'jo@theironyard.com'")
+        
+        fetchRequest.predicate = predicate
+        
+        var sortDescriptor = NSSortDescriptor(key: "date", ascending: true)
+        
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        var fetchedObjects = appD.managedObjectContext!.executeFetchRequest(fetchRequest, error: nil)
+        
+        println(fetchedObjects)
+        
     }
 
     override func didReceiveMemoryWarning() {
