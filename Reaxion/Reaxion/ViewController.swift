f223005b98e3c0fa1417bd16864069d3db0acdeb
ericstephen <EricWilliams@Erics-Macbook-Pro-3.local>
2014-09-18 10:07:42 -0400

    Reaxion - Initial Commit

---
+++ Reaxion/Reaxion/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Reaxion
+//
+//  Created by Eric Williams on 9/18/14.
+//  Copyright (c) 2014 Eric Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+        var timeBar = UIView()
+        
+        var buttons = [UIButton(), UIButton(), UIButton()]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
